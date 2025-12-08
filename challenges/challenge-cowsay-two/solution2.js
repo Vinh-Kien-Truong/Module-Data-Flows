aa83761aca48b93ce69ca489139825c125733491
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-08 23:58:25 +0100

    solution with readline

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,5 +1,13 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const topLine = "_";
+const bottomLine = "-";
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
@@ -11,8 +19,27 @@
 // 3. Make a cow that takes a string
 
 const cow = (saying) => {
-    // how did you make the cow before?
+  // how did you make the cow before?
+  saying = saying || "moo";
+  let length = saying.length;
+  let bubble = ` ${topLine.repeat(
+    length + 2
+  )} \n< ${saying} >\n ${bottomLine.repeat(
+    length + 2
+  )} \n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||`;
+
+  return bubble;
+};
+
+// 4. Use readline to get a string from the terminal
+// (with a prompt so it's clearer what we want)
+function getInput(callback) {
+  rl.question("Enter your cow message: ", (answer) => {
+    callback(answer);
+    rl.close();
+  });
 }
 
-// 4. Use readline to get a string from the terminal 
-// (with a prompt so it's clearer what we want)
+getInput((message) => {
+  console.log(cow(message));
+});

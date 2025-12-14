64f3905f3a26a83407c60d18c960ee099b66e06b
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 13:48:14 +0100

    convert to new roman test

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,30 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+  let romanNumerals = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+  };
+
+  let result = "";
+  for (let value of Object.keys(romanNumerals)
+    .map(Number)
+    .sort((a, b) => b - a)) {
+    while (n >= value) {
+      result += romanNumerals[value];
+      n -= value;
+    }
+  }
+  return result;
+}
 
 module.exports = convertToNewRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,26 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
 test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+  expect(convertToNewRoman(1)).toBe("I");
+});
+test("returns IV if passed 4 as an argument", function () {
+  expect(convertToNewRoman(4)).toBe("IV");
+});
+test("returns IX if passed 9 as an argument", function () {
+  expect(convertToNewRoman(9)).toBe("IX");
+});
+test("returns XL if passed 40 as an argument", function () {
+  expect(convertToNewRoman(40)).toBe("XL");
+});
+test("returns XC if passed 90 as an argument", function () {
+  expect(convertToNewRoman(90)).toBe("XC");
+});
+test("returns CD if passed 400 as an argument", function () {
+  expect(convertToNewRoman(400)).toBe("CD");
+});
+test("returns CM if passed 900 as an argument", function () {
+  expect(convertToNewRoman(900)).toBe("CM");
+});
+test("returns MMMCMXCIX if passed 3999 as an argument", function () {
+  expect(convertToNewRoman(3999)).toBe("MMMCMXCIX");
 });

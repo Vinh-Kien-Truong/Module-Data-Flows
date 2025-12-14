79e8616ec7535dc6c01bbbf0766f9edf95611c4b
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 13:44:51 +0100

    convert to old roman test

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,24 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+  let romanNumerals = {
+    1000: "M",
+    500: "D",
+    100: "C",
+    50: "L",
+    10: "X",
+    5: "V",
+    1: "I",
+  };
+
+  let result = "";
+  for (let value of Object.keys(romanNumerals)
+    .map(Number)
+    .sort((a, b) => b - a)) {
+    while (n >= value) {
+      result += romanNumerals[value];
+      n -= value;
+    }
+  }
+  return result;
+}
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,13 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
 test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+  expect(convertToOldRoman(1)).toBe("I");
+});
+
+test("returns II if passed 9 as an argument", function () {
+  expect(convertToOldRoman(9)).toBe("VIIII");
+});
+
+test("returns MMM if passed 3000 as an argument", function () {
+  expect(convertToOldRoman(3000)).toBe("MMM");
 });

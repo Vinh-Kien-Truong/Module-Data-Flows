6a6afc61be15ea15add06e7fc54fb6a36f8e0b30
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 13:29:19 +0100

    password verifier empty and uppercase letter check

--- challenges/unit-testing/katas-tdd/password-verifier/verifier.js
+++ challenges/unit-testing/katas-tdd/password-verifier/verifier.js
@@ -2,9 +2,11 @@
 You will be writing tests and implementing some verifications inside a function called 'verify' that takes a password as an argument. If the conditions are met, the 'verify' function should return 'Password accepted', otherwise the 'verify' function should return 'Password rejected'.
 
 - If the password is less than 8 characters, the 'verify' function should return 'Password rejected'.
+If the password does not have at least 1 uppercase letter, the 'verify' function should return 'Password rejected'. The requirements from steps 1 & 2 remain the same.
+
 */
 function verify(password) {
-  if (password.length < 8) {
+  if (password.length < 8 || !password || !/[A-Z]/.test(password)) {
     return "Password rejected";
   }
   return "Password accepted";
--- challenges/unit-testing/katas-tdd/password-verifier/verifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/verifier.test.js
@@ -6,3 +6,14 @@ test("returns Password rejected for password with less than 8 characters", funct
 test("returns Password accepted for password with 8 or more characters", function () {
   expect(verify("Ab1!efgh")).toBe("Password accepted");
 });
+test("returns Password rejected for empty password", function () {
+  expect(verify("")).toBe("Password rejected");
+});
+
+test("returns Password rejected for password without uppercase letters", function () {
+  expect(verify("ab1!efgh")).toBe("Password rejected");
+});
+
+test("returns Password accepted for password with at least one uppercase letter", function () {
+  expect(verify("Ab1!efgh")).toBe("Password accepted");
+});

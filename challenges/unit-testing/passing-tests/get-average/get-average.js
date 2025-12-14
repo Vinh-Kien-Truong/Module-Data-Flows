f9e569976f75be0a439cf437fac9760e9ccdd5a0
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 13:57:43 +0100

    get average implementation

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    const nums = numbers.filter(item => typeof item === 'number');
+    const sum = nums.reduce((acc, curr) => acc + curr, 0);
+    return nums.length === 0 ? 0 : sum / nums.length;   
+}
 
 module.exports = average;

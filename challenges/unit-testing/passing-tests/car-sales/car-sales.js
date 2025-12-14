46c0b02d116052528adf400812d85f7082348e77
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 13:52:11 +0100

    car sales implementation

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,13 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let totals = {};
+  for (let car of carsSold) {
+    if (totals[car.make]) {
+      totals[car.make] += car.price;
+    } else {
+      totals[car.make] = car.price;
+    }
+  }
+  return totals;
+}
 
 module.exports = sales;

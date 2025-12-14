126696b03f1f08cda23447a88ef88e5fd52bfff9
Truong Vinh Kien <kientruong8543@gmail.com>
2025-12-14 14:00:51 +0100

    writing test largest number test

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -1,9 +1,10 @@
 let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  let input = [3, 21, 88, 4, 36];
+  let expected = 88;
+  let result = getLargestNumber(input);
+  expect(result).toBe(expected);
 });
 
 // example
@@ -11,3 +12,10 @@ test("returns largest number in array", function () {
 // expected: 88;
 
 // also test that the original array hasn't changed
+
+test("does not modify the original array", function () {
+  let input = [3, 21, 88, 4, 36];
+  let inputCopy = [...input]; // create a copy of the original array
+  getLargestNumber(input);
+  expect(input).toEqual(inputCopy);
+});
